0d6c154c1c497623068ff93f93f55520c1716d50
shini-tm <31591116+shini-tm@users.noreply.github.com>
2022-06-27 19:25:48 +0100

    changed front page

--- pages/index.js
+++ pages/index.js
@@ -39,8 +39,8 @@ export default function Home({ posts, locale, availableLocales }) {
           <UnchartedRing />
         </div>
         <h1 className="py-3 text-4xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl xl:text-6xl md:leading-14">
-          {t('common:hi')} <span className="animate-fade-text">Mar</span>
-          <span>celo</span> <span className="animate-fade-text">Formentão</span>
+          {t('common:hi')} <span className="animate-fade-text">the </span>
+          <span>ivy.tools</span> <span className="animate-fade-text">team</span>
         </h1>
         <p className="px-2 text-xl font-light leading-6 text-gray-500 dark:text-gray-400 sm:px-6 xl:px-0">
           {t('common:mini-bio')}
